876d248a61c60c33779c7b34bfc98f02f1fbfb7b
Rick Rieger <rickrieger36251@gmail.com>
2021-03-17 12:26:25 -0400

    I did finish this work before solution presented

--- script.js
+++ script.js
@@ -12,21 +12,55 @@ const BALL_SIZE = 20;
 // Get the computer paddle element
 const computerPaddle = document.querySelector('.computer-paddle');
 
+// Get the ball element
+const ball = document.querySelector('.ball')
+
+//  ball x and y position and velocity
+let ballXPosition = 0;
+let ballYPosition = 0;
+let ballXVelocity = 3;
+let ballYVelocity = 1;
+
 // Initial computer paddle y-position and y-velocity
 let computerPaddleYPosition = 0;
-let computerPaddleYVelocity = 1;
+let computerPaddleYVelocity = 2;
+
+
 
 // Update the pong world
 function update() {
 
+    if (computerPaddleYVelocity === -2 && computerPaddleYPosition <= 0){
+        computerPaddleYVelocity = 2;
+    }
+    else if (computerPaddleYPosition >= (GAME_AREA_HEIGHT - PADDLE_HEIGHT)){
+        computerPaddleYVelocity = -2
+    }
+
+
     // Update the computer paddle's position
+
     computerPaddleYPosition = computerPaddleYPosition + computerPaddleYVelocity;
 
+    // if (computerPaddleYPosition === GAME_AREA_HEIGHT - PADDLE_HEIGHT){
+    //     computerPaddleYPosition = computerPaddleYPosition - computerPaddleYVelocity;
+    // }
+   console.log(computerPaddleYPosition);
+    // Update the ball's position
+    ballXPosition = ballXPosition + ballXVelocity;
+    ballYPosition = ballYPosition + ballYVelocity;
+
     // If the computer paddle goes off the edge of the screen, bring it back
-    computerPaddleYPosition = computerPaddleYPosition % (GAME_AREA_HEIGHT - PADDLE_HEIGHT);
+    // computerPaddleYPosition = computerPaddleYPosition % (GAME_AREA_HEIGHT - PADDLE_HEIGHT);
+    ballXPosition = ballXPosition % (GAME_AREA_WIDTH - BALL_SIZE)
+    ballYPosition = ballYPosition % (GAME_AREA_HEIGHT - BALL_SIZE)
+
+
 
     // Apply the y-position 
     computerPaddle.style.top = `${computerPaddleYPosition}px`;
+    ball.style.left = `${ballXPosition}px`;
+    ball.style.top = `${ballYPosition}px`;
 }
 
 // Call the update() function everytime the browser is ready to re-render
